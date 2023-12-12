f11942ee217b1cb819674a7b178e01f1e8c05899
carolin-violet <2976008778@qq.com>
2023-12-12 20:59:30 +0800

    [feat] 导航菜单

--- src/views/navigation/info/index.vue
+++ src/views/navigation/info/index.vue
@@ -4,36 +4,45 @@
 
     <div class="navigation">
       <div class="top-panel">
-        <a-button type="primary">
+        <a-space>
+          <a-button type="primary">
           <template #icon>
             <icon-plus-circle />
           </template>
           <template #default>添加</template>
         </a-button>
+        <a-button type="primary">
+          <template #icon>
+            <icon-plus-circle />
+          </template>
+          <template #default>导入</template>
+        </a-button>
+        <a-button type="primary">
+          <template #icon>
+            <icon-plus-circle />
+          </template>
+          <template #default>导出</template>
+        </a-button>
+        </a-space>
         <a-space style="float: right">
-          <a-cascader :options="options" :style="{ width: '320px' }" placeholder="Please select ..." />
+          <!-- <a-cascader :options="options" :style="{ width: '320px' }" placeholder="Please select ..." /> -->
+          <a-cascader :options="options" placeholder="Please select ..." />
           <a-button>筛选</a-button>
         </a-space>
       </div>
 
       <a-divider />
 
-      <a-grid :cols="{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }" :colGap="12" :rowGap="16" class="grid-demo-grid">
-        <a-grid-item class="demo-item">
-          <a-link :loading="loading2" @click="handleClick">
-            <template #icon>
+      <a-grid :cols="{ xs: 1, sm: 2, md: 3, lg: 4, xl: 6, xxl: 8 }" :colGap="12" :rowGap="16" class="grid-demo-grid">
+        <a-grid-item class="demo-item" v-for="item in list" :key="item.id">
+          <a-link :loading="loading2" :href="item.url" @click="handleClick" :title="item.description">
+            <a-space>
+              <img :src="`https://www.google.com/s2/favicons?domain=${item.url}`" alt="" width="14" height="14">
+              <span :style="{ color: item.ladder ? 'red' : 'blue' }">{{ item.title }}</span>
               <icon-edit />
-            </template>
-            Link
+              <icon-delete />
+            </a-space>
           </a-link>
-        </a-grid-item>
-        <a-grid-item class="demo-item">item</a-grid-item>
-        <a-grid-item class="demo-item">item</a-grid-item>
-        <a-grid-item class="demo-item">item</a-grid-item>
-        <a-grid-item class="demo-item">item</a-grid-item>
-        <a-grid-item class="demo-item">item</a-grid-item>
-        <a-grid-item class="demo-item" :span="{ xl: 4, xxl: 6 }" suffix>
-          suffix
         </a-grid-item>
       </a-grid>
     </div>
@@ -41,6 +50,19 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+const list = ref([
+  { id: 1, title: 'xxxxxxxxxxxxxxx', url: 'baidu.com', description: 'xxx', ladder: true },
+  { id: 2, title: 'xxxxxxxxxxxxxxx', url: 'baidu.com', description: 'xxx', ladder: false },
+  { id: 3, title: 'xxxxxxxxxxxxxxx', url: 'baidu.com', description: 'xxx', ladder: false },
+  { id: 4, title: 'xxxxxxxxxxxxxxx', url: 'baidu.com', description: 'xxx', ladder: false },
+  { id: 5, title: 'xxxxxxxxxxxxxxx', url: 'baidu.com', description: 'xxx', ladder: false },
+  { id: 6, title: 'xxxxxxxxxxxxxxx', url: 'baidu.com', description: 'xxx', ladder: true },
+  { id: 7, title: 'xxxxxxxxxxxxxxx', url: 'baidu.com', description: 'xxx', ladder: true },
+  { id: 8, title: 'xxxxxxxxxxxxxxx', url: 'baidu.com', description: 'xxx', ladder: false },
+  { id: 9, title: 'xxxxxxxxxxxxxxx', url: 'baidu.com', description: 'xxx', ladder: false },
+])
 
 </script>
 
@@ -57,19 +79,10 @@
   }
 }
 
-.grid-demo-grid .demo-item,
-.grid-demo-grid .demo-suffix {
+.grid-demo-grid .demo-item {
   height: 48px;
   line-height: 48px;
   color: var(--color-white);
   text-align: center;
-}
-
-.grid-demo-grid .demo-item:nth-child(2n) {
-  background-color: rgba(var(--arcoblue-6), 0.9);
-}
-
-.grid-demo-grid .demo-item:nth-child(2n + 1) {
-  background-color: var(--color-primary-light-4);
 }
 </style>
