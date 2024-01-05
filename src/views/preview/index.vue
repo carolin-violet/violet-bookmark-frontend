d2ac4a63535a945a72344fc7de7790547f51d464
carolin-violet <2976008778@qq.com>
2024-01-05 17:52:04 +0800

    [feat] 添加预览页

--- src/views/preview/index.vue
+++ src/views/preview/index.vue
@@ -1,17 +1,55 @@
 <template>
-  <div>ttttttt</div>
+  <div class="preview-container">ttttttt</div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue';
-import { getCategoryList } from '@/api/category'
-import { getNavigationList } from '@/api/navigation'
+import { onMounted, ref } from 'vue';
+import { exportData } from '@/api/file'
 
-const getAllCategory = () => {
+const navData = ref([])
 
+const getData = () => {
+  exportData().then((res: any) => {
+    navData.value = res.data
+  })
 }
 
 onMounted(() => {
-
+  getData()
 })
 </script>
+
+<style lang="less" scoped>
+.preview-container {
+  width: 100vw;
+  height: 100vh;
+  background-image: url('@/assets/images/bg/1080p.webp');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+
+@media screen and (max-width: 2560px) {
+  .preview-container {
+    background-image: url('@/assets/images/bg/2k.webp');
+  }
+}
+
+@media screen and (max-width: 1920px) {
+  .preview-container {
+    background-image: url('@/assets/images/bg/1080p.webp');
+  }
+}
+
+@media screen and (max-width: 1200px) {
+  .preview-container {
+    background-image: url('@/assets/images/bg/ipad2020.webp');
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .preview-container {
+    background-image: url('@/assets/images/bg/mobile.webp');
+  }
+}
+</style>
