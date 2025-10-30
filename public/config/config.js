91c891f49d70f414f36fc6088ac761656d94036e
zhoujie <2976008778@qq.com>
2025-10-30 14:33:38 +0800

    chore: 配置修改

--- public/config/config.js
+++ public/config/config.js
@@ -1,5 +1,5 @@
 const extraConfig = {
-  userCenterUrl: 'http://user-center.carolin-violet.cn:8000',
+  userCenterUrl: 'http://user-center.carolin-violet.cn:8000/register',
 };
 
 window.extraConfig = extraConfig;
